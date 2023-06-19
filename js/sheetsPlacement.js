0cf069c987c0ffe5c02647d16e3997d57ebcc9ba
KampWskiR <78373090+KampWskiR@users.noreply.github.com>
2023-06-19 21:05:01 +0200

    3

--- js/sheetsPlacement.js
+++ js/sheetsPlacement.js
@@ -31,13 +31,13 @@ function displayResult2(response) {
 
     switch (trend) {
       case '-1':
-        trendimg = "../assets/decrease.png";
+        trendimg = "assets/decrease.png";
         break;
       case '0':
-        trendimg = "../assets/stable.png";
+        trendimg = "assets/stable.png";
         break;
       case '1':
-        trendimg = "../assets/rise.png";
+        trendimg = "assets/rise.png";
         break;
     }
 
@@ -47,15 +47,15 @@ function displayResult2(response) {
     switch (true) {
       case petname === "Cowboy Capybara" || petname === "King Corgi" || petname === "Red Dragon" || petname === "UFO Capybara" || petname === "UFO Cow" || petname === "Cute o Bot":
         tableBody += `<p class='petname ${type}'>${petname}<a class='trendbox'><img class='trend' src='${trendimg}'></a></p>`;
-        tableBody += `<p order='${index}' sortable='true' id='number' type='Value' min='${minvalue}' max='${maxvalue}' valuesort='${value}' value='${value}'><img class='gem' src='../assets/gem.png'></p>`;
-        tableBody += `<p id='number' type='Silver' min='${minvalue * 5}' max='${maxvalue * 5}' value='${value * 5}'><img class='gem' src='../assets/gem.png'></p>`;
-        tableBody += `<p id='number' type='Golden' min='${minvalue * 25}' max='${maxvalue * 25}' value='${value * 25}'><img class='gem' src='../assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' sortable='true' id='number' type='Value' min='${minvalue}' max='${maxvalue}' valuesort='${value}' value='${value}'><img class='gem' src='assets/gem.png'></p>`;
+        tableBody += `<p id='number' type='Silver' min='${minvalue * 5}' max='${maxvalue * 5}' value='${value * 5}'><img class='gem' src='assets/gem.png'></p>`;
+        tableBody += `<p id='number' type='Golden' min='${minvalue * 25}' max='${maxvalue * 25}' value='${value * 25}'><img class='gem' src='assets/gem.png'></p>`;
         tableBody += `<p id='demand' demand='${demand}'/>`;
         break;
 
       case type === "limited" || petname === "Beta Cat":
         tableBody += `<p class='petname ${type}'>${petname}<a class='trendbox'><img class='trend' src='${trendimg}'></a></p>`;
-        tableBody += `<p order='${index}' sortable='true' id='number' type='Value' min='${minvalue}' max='${maxvalue}' valuesort='${value}' value='${value}'><img class='gem' src='../assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' sortable='true' id='number' type='Value' min='${minvalue}' max='${maxvalue}' valuesort='${value}' value='${value}'><img class='gem' src='assets/gem.png'></p>`;
         tableBody += `<p id='demand' demand='${demand}'/>`;
         break;
 
@@ -64,9 +64,9 @@ function displayResult2(response) {
         tableBody += `<p class='strength silvers'>${strength * 3}</p>`;
         tableBody += `<p class='strength goldens'>${strength * 9}</p>`;
         tableBody += `<p class='petname ${type}'>${petname}<a class='trendbox'><img class='trend' src='${trendimg}'></a></p>`;
-        tableBody += `<p order='${index}' sortable='true' class='boostergui' id='number' type='Normal' min='${minvalue}' max='${maxvalue}' valuesort='${value}' value='${value}'><img class='gem' src='../assets/gem.png'></p>`;
-        tableBody += `<p order='${index}' class='boostergui' id='number' type='Silver' min='${minvalue * 3}' max='${maxvalue * 3}' value='${value * 3}'><img class='gem' src='../assets/gem.png'></p>`;
-        tableBody += `<p order='${index}' class='boostergui' id='number' type='Golden' min='${minvalue * 9}' max='${maxvalue * 9}' value='${value * 9}'><img class='gem' src='../assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' sortable='true' class='boostergui' id='number' type='Normal' min='${minvalue}' max='${maxvalue}' valuesort='${value}' value='${value}'><img class='gem' src='assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' class='boostergui' id='number' type='Silver' min='${minvalue * 3}' max='${maxvalue * 3}' value='${value * 3}'><img class='gem' src='assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' class='boostergui' id='number' type='Golden' min='${minvalue * 9}' max='${maxvalue * 9}' value='${value * 9}'><img class='gem' src='assets/gem.png'></p>`;
         break;
 
       case type === "secret":
@@ -74,9 +74,9 @@ function displayResult2(response) {
         tableBody += `<p class='strength silvers'>${strength * 5}</p>`;
         tableBody += `<p class='strength goldens'>${strength * 25}</p>`;
         tableBody += `<p class='petname ${type}'>${petname}<a class='trendbox'><img class='trend' src='${trendimg}'></a></p>`;
-        tableBody += `<p order='${index}' sortable='true' class='boostergui' id='number' type='Normal' min='${minvalue}' max='${maxvalue}' valuesort='${value}' value='${value}'><img class='gem' src='../assets/gem.png'></p>`;
-        tableBody += `<p order='${index}' class='boostergui' id='number' type='Silver' min='${minvalue * 3}' max='${maxvalue * 3}' value='${value * 3}'><img class='gem' src='../assets/gem.png'></p>`;
-        tableBody += `<p order='${index}' class='boostergui' id='number' type='Golden' min='${minvalue * 9}' max='${maxvalue * 9}' value='${value * 9}'><img class='gem' src='../assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' sortable='true' class='boostergui' id='number' type='Normal' min='${minvalue}' max='${maxvalue}' valuesort='${value}' value='${value}'><img class='gem' src='assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' class='boostergui' id='number' type='Silver' min='${minvalue * 3}' max='${maxvalue * 3}' value='${value * 3}'><img class='gem' src='assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' class='boostergui' id='number' type='Golden' min='${minvalue * 9}' max='${maxvalue * 9}' value='${value * 9}'><img class='gem' src='assets/gem.png'></p>`;
         break;
 
       default:
@@ -84,9 +84,9 @@ function displayResult2(response) {
         tableBody += `<p class='strength silvers'>${strength * 3}</p>`;
         tableBody += `<p class='strength goldens'>${strength * 9}</p>`;
         tableBody += `<p class='petname ${type}'>${petname}<a class='trendbox'><img class='trend' src='${trendimg}'></a></p>`;
-        tableBody += `<p order='${index}' sortable='true' id='number' type='Normal' valuesort='${value}' min='${minvalue}' max='${maxvalue}' value='${value}'><img class='gem' src='../assets/gem.png'></p>`;
-        tableBody += `<p order='${index}' id='number' type='Silver' min='${minvalue * 3}' max='${maxvalue * 3}' value='${value * 3}'><img class='gem' src='../assets/gem.png'></p>`;
-        tableBody += `<p order='${index}' id='number' type='Golden' min='${minvalue * 9}' max='${maxvalue * 9}' value='${value * 9}'><img class='gem' src='../assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' sortable='true' id='number' type='Normal' valuesort='${value}' min='${minvalue}' max='${maxvalue}' value='${value}'><img class='gem' src='assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' id='number' type='Silver' min='${minvalue * 3}' max='${maxvalue * 3}' value='${value * 3}'><img class='gem' src='assets/gem.png'></p>`;
+        tableBody += `<p order='${index}' id='number' type='Golden' min='${minvalue * 9}' max='${maxvalue * 9}' value='${value * 9}'><img class='gem' src='assets/gem.png'></p>`;
         break;
     }
         if (main === "potd" && window.location.pathname === "/pets/") tableBody += '<h1 id="rainbow">Pet Of The Day!</h1>'; tableBody += '<script src="js/potd.js"></script>';
