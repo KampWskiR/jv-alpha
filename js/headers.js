bf1bb32898d7b74701a43cf6effbc6d015df1622
KampWskiR <78373090+KampWskiR@users.noreply.github.com>
2023-06-20 21:02:10 +0200

    1

--- js/headers.js
+++ js/headers.js
@@ -2,8 +2,8 @@ let header = "";
 
 header += `<img class="logo" src="assets/LogoNew.png" onclick="window.location.href='../'">`;
 header += `<div class="nav">`;
-  header += `<a class="NavButton NBPets basic" href="pets"">Pets</a>`;
-  header += `<a class="NavButton NBMounts blue" href="mounts"">Mounts</a>`;
+  header += `<a class="NavButton NBPets basic" href="pets">Pets</a>`;
+  header += `<a class="NavButton NBMounts blue" href="mounts">Mounts</a>`;
       header += `<div class="NavButton NBMore">`;
       header += `<p>More</p>`;
       header += `<ul class="list">`;
